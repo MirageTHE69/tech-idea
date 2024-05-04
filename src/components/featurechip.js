ea0ece2e4e7f83c0edc6790bacd1c7dee5399f3a
unknown <mthakore12@gmail.com>
2024-05-05 01:50:21 +0530

    doneeeeeeeeeeeeeeeeeeeeeeeeee

--- src/components/featurechip.js
+++ src/components/featurechip.js
@@ -2,10 +2,11 @@ import React from "react";
 
 function Featurechip({ text }) {
   return (
-    <div className="feature-chip">
-      <span>{text}</span>
-    </div>
+    <div className="flex h-[10vh] lg:h-[10vh] items-center justify-center text-center text-white rounded-3xl" style={{ background: 'linear-gradient(135deg, #0D224B 0%, #04080f 100%)' }}>
+    <span className="">{text}</span>
+  </div>
+  
   );
 }
 
-export default Featurechip;
+export default Featurechip
