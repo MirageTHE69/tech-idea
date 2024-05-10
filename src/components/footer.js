f946bc38651dbc722d2a608827b5abd0ffd05f3e
unknown <mthakore12@gmail.com>
2024-05-10 19:30:47 +0530

    today addedd3

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,7 +20,7 @@ function Footer() {
 
         <div className="w-full  flex flex-col sm:flex-row justify-between items-center mt-8">
           <span className="text-xs text-center sm:text-left">
-            © 2024 Tech-idea. All Rights Reserved. made by innvoket{" "}
+            © 2024 Tech-idea. All Rights Reserved
             <a href="https://innvoket.com" className="text-white">
               innvoket.com
             </a>
